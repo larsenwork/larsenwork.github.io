df117902de9bd560f8ad15ed50df175cbc0b3e1b
larsenwork <andreas@larsenwork.com>
2018-01-28 14:03:39 +0100

    Fix some iOS issues

--- components/thank-you.vue
+++ components/thank-you.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h2>Thank you</h2>
-    <p>Help me by <a :href="tweetLink">tweeting about it</a>, <a :href="`https://github.com/larsenwork/${repo}`">star/contribute on GitHub</a> or <a href="https://www.crowdrise.com/larsenwork-paying-it-forward">donate to my foundraiser</a> for Ethiopia Health Aid Inc.</p>
+    <p>Help me by
+      <a :href="`https://twitter.com/intent/tweet?text=${twitterText}&url=${twitterUrl}`">tweeting about it</a>,
+      <a :href="`https://github.com/larsenwork/${repo}`">star/contribute on GitHub</a>
+      or
+      <a href="https://www.crowdrise.com/larsenwork-paying-it-forward">donate to my foundraiser</a>
+      for Ethiopia Health Aid Inc.
+    </p>
   </div>
 </template>
 
@@ -22,10 +28,11 @@ export default {
     }
   },
   computed: {
-    tweetLink () {
-      const text = encodeURIComponent(this.tweetText + ' by @larsenwork')
-      const url = encodeURIComponent('https://larsenwork' + this.path)
-      return `https://twitter.com/intent/tweet?text=${text}&url=${url}`
+    twitterText () {
+      return encodeURI(`${this.tweetText} by @larsenwork`)
+    },
+    twitterUrl () {
+      return encodeURI(`https://larsenwork${this.path}`)
     }
   }
 }
