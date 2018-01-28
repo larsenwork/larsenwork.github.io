85744e956d4893c9241c53b009d327fce2ea8963
larsenwork <andreas@larsenwork.com>
2018-01-28 09:32:12 +0100

    Less text

--- pages/easing-gradients.vue
+++ pages/easing-gradients.vue
@@ -12,13 +12,13 @@
       <gradient-examples></gradient-examples>
       <div id="editor"></div>
       <h2>Editor</h2>
-      <p>Create and preview your own easing gradients in CSS. Note that the CSSWG proposal syntax isn't supported in any browsers yet.</p>
+      <p>Create and preview your own easing gradients in CSS. Note that the CSSWG syntax isn't supported in any browsers yet.</p>
       <gradient-editor></gradient-editor>
       <h2>Explained</h2>
       <p>I wrote about it in detail on <a href="https://css-tricks.com/easing-linear-gradients/">CSS-Tricks</a>.</p>
       <p>Fortunately other people liked my idea and <a href="https://twitter.com/meyerweb/status/861614513371664384">Eric Meyer</a> made a <a href="https://github.com/w3c/csswg-drafts/issues/1332">CSSWG proposal</a> where the syntax was refined and you can chime in with your ideas/suggestions.</p>
       <p>Until that happens 🤞 you can use the <a href="https://github.com/larsenwork/postcss-easing-gradients">postCSS-easing-gradients</a> plugin which is more powerfull than the editor above.</p>
-      <p>What the plugin and the editor does is adding a bunch of intermediate color-stops to create a "low poly" version of the easing function. The end result is ok and a lot prettier than plain linear gradients in relevant use cases.</p>
+      <p>What the plugin and the editor does is adding a bunch of intermediate color-stops to create a "low poly" version of the easing function. The end result is ok for now and a lot prettier in relevant use cases.</p>
     </section>
   </div>
 </template>
