4048b4e3b9decab837ee199e2b49ba32d281325b
larsenwork <andreas@larsenwork.com>
2018-02-11 08:53:24 +0400

    Add cubic bezier shothands

--- components/tools/gradient/calculations/gradient-output.js
+++ components/tools/gradient/calculations/gradient-output.js
@@ -1,6 +1,6 @@
 import easingStopsCubic from './gradient-stops-cubic'
 import easingStopsSteps from './gradient-stops-steps'
-import * as easingShorthands from './gradient-shorthands'
+import * as shorthands from './gradient-shorthands'
 import chroma from 'chroma-js'
 
 export default {
@@ -16,46 +16,56 @@ export default {
     },
     gradientFunction () {
       if (this.$store.state.gradient.settings.easingFunction === 'cubic-bezier') {
-        return `cubic-bezier(${this.getStoreBezierCoordinates().join(', ')})`
+        const cubic = `cubic-bezier(${this.getStoreBezierCoordinates().join(', ')})`
+        return shorthands.cubic[cubic]
+          ? shorthands.cubic[cubic]
+          : cubic
       } else {
         return `steps(${this.$store.state.gradient.steps.number}, ${this.$store.state.gradient.steps.skip})`
       }
     }
   },
   methods: {
+    // lrgb as default value since it produces a result closes to most browser defaults
     gradientCalc (colorMode = 'lrgb') {
+      // Because number input in Safari is a bitch...
       let stepsNumber = this.$store.state.gradient.steps.number
       if (stepsNumber === undefined || stepsNumber < 2) {
         stepsNumber = 2
       } else if (stepsNumber > 100) {
         stepsNumber = 100
       }
-      const colorStops = this.$store.state.gradient.settings.easingFunction === 'cubic-bezier'
+
+      // Get the colorstops
+      const colorStopsCoordinates = this.$store.state.gradient.settings.easingFunction === 'cubic-bezier'
         ? easingStopsCubic(this.getStoreBezierCoordinates())
         : easingStopsSteps(stepsNumber, this.$store.state.gradient.steps.skip)
-      this.$store.commit('updateStopCoordinates', colorStops)
+      this.$store.commit('updateStopCoordinates', colorStopsCoordinates)
       const colors = [
         this.getStoreHsla1(),
         this.getStoreHsla2()
       ]
+
+      // Browsers don't really agree how to treat gradients that go from one hue to alpha 0 of another hue...
       const correctTransparentColors = colors.map((color, i) => {
         return chroma(color).alpha() === 0
           ? chroma(colors[Math.abs(i - 1)]).alpha(0)
           : chroma(color)
       })
-      const cssColorStops = colorStops.map(stop => {
+
+      // Mix them colors and write it as sensible css
+      const cssColorStops = colorStopsCoordinates.map(stop => {
         let mixedColor = chroma.mix(...correctTransparentColors, stop.mix, colorMode)
         mixedColor = mixedColor.alpha(rounded(mixedColor.alpha(), 3))
-        return `${mixedColor.css('hsl').split(',').join(', ')} ${+(stop.position * 100).toFixed(1)}%`
+        return `${mixedColor.css('hsl').split(',').join(', ')} ${rounded(stop.position * 100, 1)}%`
       })
       const direction = this.getStoreDirection()
-      const gradientCSS = `linear-gradient(${direction}, ${cssColorStops.join(', ')})`
-      return gradientCSS
+      return `linear-gradient(${direction}, ${cssColorStops.join(', ')})`
     },
     getStoreDirection () {
       const deg = rounded(this.$store.state.gradient.direction.deg)
-      const str = easingShorthands.direction[deg]
-        ? easingShorthands.direction[deg]
+      const str = shorthands.direction[deg]
+        ? shorthands.direction[deg]
         : `${deg}deg`
       return str
     },
--- components/tools/gradient/calculations/gradient-shorthands.js
+++ components/tools/gradient/calculations/gradient-shorthands.js
@@ -11,5 +11,8 @@ export const direction = {
 }
 
 export const cubic = {
-
+  'cubic-bezier(0.42, 0, 0.58, 1)': 'ease-in-out',
+  'cubic-bezier(0, 0, 0.58, 1)': 'ease-out',
+  'cubic-bezier(0.42, 0, 1, 1)': 'ease-in',
+  'cubic-bezier(0.25, 0.1, 0.25, 1)': 'ease'
 }
