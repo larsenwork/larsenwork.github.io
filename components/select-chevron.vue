fcd024204b868ee7d9b80b80425375faa9864acb
larsenwork <andreas@larsenwork.com>
2018-05-31 06:49:05 +0200

    Preload font

---
+++ components/select-chevron.vue
@@ -0,0 +1,25 @@
+<template>
+  <chevron-down-icon
+    class="u-icon c-selectChevron"
+  />
+</template>
+
+<script>
+import { ChevronDownIcon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    ChevronDownIcon,
+  },
+}
+</script>
+
+<style>
+.c-selectChevron {
+  position: absolute;
+  right: var(--spacer-xsmall);
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+</style>
